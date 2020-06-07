aea0f55da2d78cede45d895384f487142f21019c
liubasara <followliu@163.com>
2020-06-07 23:03:15 +0800

    优化寻找字符串脚本

--- findstr.sh
+++ findstr.sh
@@ -1,3 +1,8 @@
-echo -n "输入你要查找的字符串："
-read find_str
+if [ -n "$1" ]
+then
+  find_str="$1"
+else
+  echo -n "输入你要查找的字符串："
+  read find_str
+fi
 echo $(find . -type f -name "*" | xargs grep -s -n "$find_str")
