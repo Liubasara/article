e42a172474b899fc47194105494319306c42b8ef
liubasara <followliu@163.com>
2020-12-06 13:55:29 +0800

    update 12/6 文摘

--- findstr.sh
+++ findstr.sh
@@ -5,4 +5,10 @@ else
   echo -n "输入你要查找的字符串："
   read find_str
 fi
-echo $(find . -type f -name "*" | xargs grep -s -n "$find_str")
+
+IFS=$'\n'
+
+for res in $(find . -type f -name "*" | xargs grep -s -n "$find_str")
+do
+  echo "$res"
+done
