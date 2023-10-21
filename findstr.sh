d7cc7334ddfcb5c91902eea40de241cae5a306c6
liubasara <followliu@163.com>
2023-10-21 10:47:37 +0800

    feat: 优化查找脚本

--- findstr.sh
+++ findstr.sh
@@ -1,21 +1,18 @@
-if [ -n "$1" ]
-then
-  find_str="$1"
+if [ -n "$1" ]; then
+  if [ -n "$2" ]; then
+    find_path="$1"
+    find_str="$2"
+  else
+    find_path="."
+    find_str="$1"
+  fi
 else
-  echo -n "输入你要查找的字符串："
-  read find_str
-fi
-
-if [ -n "$2" ]
-then
-  find_path="$2"
-else
+  read -p "请输入要查找的字符串: " find_str
   find_path="."
 fi
 
 IFS=$'\n'
 
-for res in $(find $find_path -type f -name "*" | xargs grep -s -n "$find_str")
-do
+for res in $(find $find_path -type f -name "*" | xargs grep -s -n "$find_str"); do
   echo "$res"
 done
